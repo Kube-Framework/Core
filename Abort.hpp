8d2a07acee89c0fed387ffa237838e023a3513db
MatthieuMv <m.moinvaziri@gmail.com>
2023-08-22 01:12:39 +0200

    [Feat] Abort -> terminate

--- Abort.hpp
+++ Abort.hpp
@@ -8,8 +8,8 @@
 #include "Log.hpp"
 
 /** @brief Write a message to the error log then abort the program */
-#define kFAbort(...)                    __VA_OPT__(kFError(__VA_ARGS__),) std::abort()
-#define kFAbortRaw(...)                 __VA_OPT__(kFErrorRaw(__VA_ARGS__),) std::abort()
+#define kFAbort(...)                    __VA_OPT__(kFError(__VA_ARGS__),) std::terminate()
+#define kFAbortRaw(...)                 __VA_OPT__(kFErrorRaw(__VA_ARGS__),) std::terminate()
 
 /** @brief Test an expression and abort if it fails */
 #define kFEnsure(expression, ...)       if (!(expression)) [[unlikely]] kFAbort(__VA_OPT__(__VA_ARGS__))
